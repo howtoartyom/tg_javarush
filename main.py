06a4405157f9251a56fdb2100ea27ba8630b619a
a.getashvili <a.getashvili@1440.space>
2025-05-29 20:11:35 +0300

    add chatgpt interface to bot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, filters
 from config import TG_BOT_TOKEN
-from handlers import basic, random_fact
+from handlers import basic, random_fact, chatgpt_interface
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -12,11 +12,26 @@ logger = logging.getLogger(__name__)
 def main():
     try:
         application = Application.builder().token(TG_BOT_TOKEN).build()
+
         application.add_handler(CommandHandler("start", basic.start))
-
         application.add_handler(CommandHandler("random", random_fact.random_fact))
-        application.add_handler(CallbackQueryHandler(random_fact.random_fact_callback, pattern="^random_"))
+        application.add_handler(CommandHandler("gpt", chatgpt_interface.gpt_command))
 
+        gpt_conversation = ConversationHandler(
+            entry_points=[CallbackQueryHandler(chatgpt_interface.gpt_start, pattern="^gpt_interface$")],
+            states={
+                chatgpt_interface.WAITING_FOR_MESSAGE: [
+                    MessageHandler(filters.TEXT & ~filters.COMMAND, chatgpt_interface.handle_gpt_message)
+                ],
+            },
+            fallbacks=[
+                CommandHandler("start", basic.start),
+                CallbackQueryHandler(basic.menu_callback, pattern="^(gpt_finish|main_menu)$")
+            ],
+        )
+
+        application.add_handler(gpt_conversation)
+        application.add_handler(CallbackQueryHandler(random_fact.random_fact_callback, pattern="^random_"))
         application.add_handler(CallbackQueryHandler(basic.menu_callback))
 
         logger.info("Бот запущен успешно!")
